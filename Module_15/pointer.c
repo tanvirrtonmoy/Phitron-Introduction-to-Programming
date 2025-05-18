fb41573a66124a366b89299e67a6bd7d21281479
tanvirrtonmoy <tanvirrtonmoy@gmail.com>
2025-05-19 02:21:05 +0600

    pointer edited

--- Module_15/pointer.c
+++ Module_15/pointer.c
@@ -9,6 +9,7 @@ int main()
     ptr=&x;
     printf("%p\n",ptr);
     printf("%p\n",&ptr);
+    /// dsgrgfd
 
     // onno akta variable ar address print kore
     //pointer ar oo address tahakaaa poree dakboo
